88b0197c497bca608dd77f901f19919b412bcef7
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-16 14:03:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.
+      @@cart.empty?
+      resp.write "Your cart is empty"
     else
       resp.write "Path Not Found"
     end
