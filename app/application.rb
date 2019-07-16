2973177643dbb36db2a0875f0458a633b3fd2012
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-16 14:10:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty"
       else 
-        resp.write "#{@@cart}.join("")"
+        resp.write "#{@@cart.to_s}"
       end
     else
       resp.write "Path Not Found"
